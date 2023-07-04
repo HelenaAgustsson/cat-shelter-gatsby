e4cc162fa3e54d309e53d56af252e7dfcf93e458
Helena Agustsson <agustssonhelena@gmail.com>
2023-07-04 16:01:20 +0100

    finished setting up fresh template

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,7 @@
 import React from 'react'
-import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <Layout>
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <h1>404: Not Found</h1>
-      <p>No page here.</p>
-    </div>
-  </Layout>
+  <>Np page here</>
 )
 
 export default NotFoundPage
