83b7be2891fe5a357ed515eb967f97ff7639151c
Helena Agustsson <agustssonhelena@gmail.com>
2023-09-08 12:09:32 +0100

    styled header with styled components

---
+++ src/components/layout/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const HeaderDiv = styled.div`
+    background: #9175a8;
+    color: #ffffff;
+    margin-bottom: 1.45rem;
+    font-weight: bold;
+`;
+
+export const FlexContainer = styled.div`
+    margin: 0 auto;
+    max-width: 960px;
+    display: flex;
+    justify-items: space-between;
+    align-items: center;
+`;
+
+export const TitleContainer = styled.div`
+    margin: 0; 
+    flex: 1;
+`;
+
+export const LinksContainer = styled.ul`
+    display: flex;
+    flex: 1;
+`;
