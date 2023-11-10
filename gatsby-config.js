30bf3fdd4d3be356bf595808e5660cf3ebaa7585
Helena Agustsson <agustssonhelena@gmail.com>
2023-11-10 15:05:53 +0000

    added styled components to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: "@sentry/gatsby",
     }
   ],
