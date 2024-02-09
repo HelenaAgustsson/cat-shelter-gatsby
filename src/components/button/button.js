ab7971cea5635ca16ad31f13815699d6681910a4
Helena Agustsson <agustssonhelena@gmail.com>
2024-02-09 14:43:04 +0000

    added banner and styled button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,13 +1,11 @@
-import React from "react"
+import React from 'react'
 import DefaultButton from './styles'
 
-const Button =()=>{
-    const onClick = () =>{
-        console.log('click')
-    }
-    return (
-        <DefaultButton onClick={onClick}>Click me</DefaultButton>
-    )
+const Button = ({ cta }) => {
+  const onClick = () => {
+    console.log('click')
+  }
+  return <DefaultButton onClick={onClick}>{cta}</DefaultButton>
 }
 
-export default Button;
+export default Button
--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components'
 
 const DefaultButton = styled.button`
-  font-size: 1.5em;
+  font-size: 0.9em;
   text-align: center;
-  color: #BF4F74;
-`;
+  color: #ffffff;
+  background: rgb(235, 0, 31);
+  padding: 11px 15px 12px;
+  border: none;
+  border-radius: 2px;
+  margin: 0 15px;
+  font-weight: bold;
+`
 
 export default DefaultButton
