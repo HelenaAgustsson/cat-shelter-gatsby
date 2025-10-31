a956507bf2349041f8fa49679d59291b322bc127
Helena Agustsson <agustssonhelena@gmail.com>
2025-10-31 16:50:25 +0000

    product component test

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { GatsbyImage } from 'gatsby-plugin-image'
-import RichText from '../richtext'
 import Section from '../section/section'
 import {
   ImageContainer,
@@ -10,19 +9,19 @@ import {
 } from '../product/styles'
 
 const Product = ({ productData }) => {
-  const title = productData.title
-  const image = productData.image.gatsbyImageData
-  const alt = productData.alt
+  const title = productData?.title
+  const image = productData?.image?.gatsbyImageData
+  const alt = productData?.alt
 
   return (
     <Section>
       <FlexContainer>
         <ImageContainer>
-          <GatsbyImage image={image} alt={alt} />
+          {image ? <GatsbyImage image={image} alt={alt} /> : null}
         </ImageContainer>
         <ProductDetails>
           <ProductTitle>{title}</ProductTitle>
-          <RichText richText={productData.description} />
+          {productData?.description}
         </ProductDetails>
       </FlexContainer>
     </Section>
---
+++ src/components/product/product.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Product from './product'
+
+const data = {
+  title: 'Green beanie',
+  image: '',
+  alt: 'woman in green beanie',
+  description: 'cosy beanie hat',
+}
+
+it('should render successfully', () => {
+  render(<Product productData={data} />)
+  const productElement = screen.getByText(data.description)
+  expect(productElement).toBeDefined()
+  const productHeading = screen.getByText(data.title)
+  expect(productHeading).toBeDefined()
+})
