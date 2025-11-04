fb4d7df5481cb274103f561fdf10fd385a0ce152
Helena Agustsson <agustssonhelena@gmail.com>
2025-11-04 11:30:44 +0000

    dual block responsive styling

--- src/components/dual-block/dual-block.js
+++ src/components/dual-block/dual-block.js
@@ -4,11 +4,11 @@ import { DualContainer, Header, TextContainer, ImageContainer } from './styles'
 const DualBlock = ({ data }) => {
   return (
     <DualContainer>
+      <ImageContainer $image={data.image.file.url} />
       <TextContainer>
         <Header>{data.title}</Header>
         {data.text}
       </TextContainer>
-      <ImageContainer $image={data.image.file.url} />
     </DualContainer>
   )
 }
