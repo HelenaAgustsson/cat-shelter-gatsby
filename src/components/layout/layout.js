9013051061534f437bc470d42f8d429b67f900a8
Helena Agustsson <agustssonhelena@gmail.com>
2026-07-09 11:36:42 +0200

    updated metadata and opengraph image

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,6 +3,7 @@ import { useStaticQuery, graphql } from 'gatsby'
 import SiteNav from '../navigation/sitenav'
 import Footer from './footer'
 import { LayoutDiv, PageContainer } from './styles'
+import { Helmet } from 'react-helmet'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,6 +22,23 @@ const Layout = ({ children }) => {
 
   return (
     <LayoutDiv>
+      <Helmet>
+        <meta
+          property="og:image"
+          content="https://helenas-cat-shelter.netlify.app/catshelter-screenshot.webp"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:title" content="Helena's Cat Shelter" />
+        <meta
+          property="og:description"
+          content="Helena's Cat Shelter is a non-profit organization dedicated to finding loving homes for cats in need. Browse our cats and start your adoption journey today."
+        />
+        <meta
+          property="og:url"
+          content="https://helenas-cat-shelter.netlify.app"
+        />
+      </Helmet>
       <SiteNav
         menuLinks={data.site.siteMetadata.menuLinks}
         siteTitle={data.site.siteMetadata?.title}
